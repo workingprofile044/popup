bc5c58352c9e72114f76b9390adc422ccacda215
workingprofile044 <eternalsufferingb0y@gmail.com>
2024-05-18 12:20:19 +0800

    update test

--- tests/popup.test.js
+++ tests/popup.test.js
@@ -2,10 +2,18 @@ const { JSDOM } = require('jsdom');
 const { createPopup } = require('../src/popup');
 
 test('popup creation', () => {
-    const dom = new JSDOM('<!DOCTYPE html><body></body>');
+    const dom = new JSDOM('<!DOCTYPE html><body><button id="test-button">Show Popup</button></body>');
     global.document = dom.window.document;
 
-    createPopup('Test Name', 'Test Description');
+    const button = document.getElementById('test-button');
+    button.getBoundingClientRect = () => ({
+        top: 100,
+        left: 100,
+        width: 100,
+        height: 50
+    });
+
+    createPopup('Test Name', 'Test Description', button);
 
     const popup = document.querySelector('.popup');
     expect(popup).not.toBeNull();
