5631855b7b28b8f932bde89b4ec0738d01413008
workingprofile044 <eternalsufferingb0y@gmail.com>
2024-05-18 11:59:08 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -32,5 +33,11 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+  ],
   mode: 'development',
 };
